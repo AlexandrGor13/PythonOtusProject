31cb3b867e4b9d2c0ec028bf92c34f774d15d6e7
alexandr <gorbenich@gmail.com>
2025-04-18 19:10:35 +0300

    create auth

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,4 +1,4 @@
-from .login import router as login_router
+from .auth import router as auth_router
 from .root import router as root_router
 from .users.views import router as users_router
 from .dependencies import users_crud
@@ -7,6 +7,6 @@ from fastapi import APIRouter
 
 router = APIRouter( )
 
-router.include_router(login_router)
+router.include_router(auth_router)
 router.include_router(root_router)
 router.include_router(users_router)
--- app/api/auth.py
+++ app/api/auth.py
@@ -1,13 +1,13 @@
 from typing import Annotated
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, Body, HTTPException
 
 from .users.schemas import UserAuth
 from .token import Token
 from core.security import create_jwt_token
-from .dependencies import auth_user_oath2
+from .dependencies import auth_user_oath2, oauth2_scheme
 
 router = APIRouter(tags=["Authentification"])
-
+blacklist = set()
 
 @router.post(
     "/login",
@@ -15,7 +15,7 @@ router = APIRouter(tags=["Authentification"])
     summary="User authentification",
     responses={
         status.HTTP_200_OK: {
-            "description": "User deleted",
+            "description": "User login",
             "content": {
                 "application/json": {
                     "example": {"access_token": "token", "token_type": "bearer"}
@@ -33,3 +33,32 @@ def login(
     """Функция авторизации пользователя. В случае успеха возвращает токен доступа"""
     token = create_jwt_token({"sub": user_in.username})
     return Token(access_token=token, token_type="bearer")
+
+@router.post(
+    "/logout",
+    status_code=status.HTTP_200_OK,
+    summary="User authentification",
+    responses={
+        status.HTTP_200_OK: {
+            "description": "User logout",
+            "content": {
+                "application/json": {
+                    "example": {"access_token": "token", "token_type": "bearer"}
+                }
+            },
+        },
+    },
+)
+def logout(token: Annotated[Token, Body()]):
+    blacklist.add(token.access_token)
+    return {"msg": "Successfully logged out"}
+
+
+
+
+
+@router.get("/protected")
+def protected_route(token: str = Depends(oauth2_scheme)):
+    if token in blacklist:
+        raise HTTPException(status_code=403, detail="Token has been blacklisted")
+    return {"msg": "Access granted"}
